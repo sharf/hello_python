6c3778d4485c6b1f3fad2b398af63c4911b3f834
sharf <shak.ceg@gmail.com>
2012-06-15 23:43:44 +0530

    Added details

--- setup.py
+++ setup.py
@@ -3,15 +3,26 @@ try:
 except ImportError:
     from distutils.core import setup
 
+install = []
+
+try:
+   import importlib
+except ImportError:
+   install.append("importlib")
+
 setup(name='hello_python',
       version='1.0',
       license='BSD',
       description='Pipeline library',
+      long_description='Pipeline library with steps: uppercase, date , duplicate'
       author='sharf',
       author_email='shak.ceg@gmail.com',
       url='http://github.com/sharf/hello_python',
       platforms = ['Any'],
       packages = ['pipes','pipes.lib','pipes.steps'],
+      install_requires = install,
+      tests_require = ['nose'],
+      test_suite = 'nose.collector',
       classifiers = [ 'Development Status :: 1 - Beta',
                       'License :: OSI Approved :: BSD License',
                       'Operating System :: OS Independent',
