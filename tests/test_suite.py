aaf2ee53bb6215da11e366c7ae795923ac93c212
sharf <shak.ceg@gmail.com>
2012-06-15 13:08:49 +0530

    Created TestSuite class

--- tests/test_suite.py
+++ tests/test_suite.py
@@ -2,21 +2,22 @@ from nose.tools import eq_
 from pipes.lib.pipeline import Pipeline
 import datetime
 
-def test_uppercase():
-  input = {'original': 'Hello'}
-  pipeline = Pipeline()
-  pipeline.run(input)
-  eq_(input['UCASE'],'HELLO')
+class TestSuite:
+  def test_uppercase(self):
+    input = {'original': 'Hello'}
+    pipeline = Pipeline()
+    pipeline.run(input)
+    eq_(input['UCASE'],'HELLO')
 
-def test_duplicate():
-  input = {'original': 'Hello'}
-  pipeline = Pipeline()
-  pipeline.run(input)
-  eq_(input['DUP'], 'Hello Hello')
+  def test_duplicate(self):
+    input = {'original': 'Hello'}
+    pipeline = Pipeline()
+    pipeline.run(input)
+    eq_(input['DUP'], 'Hello Hello')
 
-def test_date():
-  input = {'original': 'Hello'}
-  pipeline = Pipeline()
-  pipeline.run(input)
-  now = datetime.datetime.now()
-  eq_(input['DATE'], 'Hello'+ now.strftime(":%m-%d-%Y"))
+  def test_date(self):
+    input = {'original': 'Hello'}
+    pipeline = Pipeline()
+    pipeline.run(input)
+    now = datetime.datetime.now()
+    eq_(input['DATE'], 'Hello'+ now.strftime(":%m-%d-%Y"))
