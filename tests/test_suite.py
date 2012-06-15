c608beb0fbbb7524447b0ddc09809fcba630b20a
sharf <shak.ceg@gmail.com>
2012-06-15 13:05:26 +0530

    Added unit test for date step

--- tests/test_suite.py
+++ tests/test_suite.py
@@ -1,5 +1,6 @@
 from nose.tools import eq_
 from pipes.lib.pipeline import Pipeline
+import datetime
 
 def test_uppercase():
   input = {'original': 'Hello'}
@@ -12,3 +13,10 @@ def test_duplicate():
   pipeline = Pipeline()
   pipeline.run(input)
   eq_(input['DUP'], 'Hello Hello')
+
+def test_date():
+  input = {'original': 'Hello'}
+  pipeline = Pipeline()
+  pipeline.run(input)
+  now = datetime.datetime.now()
+  eq_(input['DATE'], 'Hello'+ now.strftime(":%m-%d-%Y"))
